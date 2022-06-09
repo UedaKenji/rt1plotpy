7f74de83922ae7a35db1133a58c06dcccdf04b89
UedaKenji <ueda.kenji19@ae.k.u-tokyo.ac.jp>
2022-06-09 13:31:54 +0900

    setupを変更

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup, find_packages
+import setuptools
 
 setup(
     name="rt1plotpy",
     version="0.1.0",
     author='Kenji Ueda',
-    author_email='kentokamak@gmail.compython setup.py install',
-    install_requires=["dxfgrabber",'opencv-python']
+    author_email='kentokamak@gmail.com',
+    url="https://github.com/UedaKenji/rt1_advanced_plot",
+    install_requires=["dxfgrabber",'opencv-python'],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.7",
 )
